4f269a83b581658cfe2e07c4e17743ecb2a3386a
CarlosAndres <solrak41@gmail.com>
2023-02-17 02:52:49 -0500

    Add styling to the first card to be displayed. Set grid to auto-fill .grid-container once mora cards are added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cardContainer = document.querySelector(".card-container");
 if (myLibrary.length === 0) {
   const firstCard = document.createElement("div");
   firstCard.textContent =
-    "Your library is empty. Click the + icon to add your first book.";
-  firstCard.classList.add("card");
+    "Your library is empty. Click on the + icon to add your first book.";
+  firstCard.classList.add("first-card");
   cardContainer.appendChild(firstCard);
 }
