4a09921967d297eeb2e6690035881f17afecc73c
CarlosAndres <solrak41@gmail.com>
2023-02-17 02:32:58 -0500

    Add a first card if myLibrary array is empty

---
+++ app.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+const cardContainer = document.querySelector(".card-container");
+
+if (myLibrary.length === 0) {
+  const firstCard = document.createElement("div");
+  firstCard.textContent =
+    "Your library is empty. Click the + icon to add your first book.";
+  firstCard.classList.add("card");
+  cardContainer.appendChild(firstCard);
+}
