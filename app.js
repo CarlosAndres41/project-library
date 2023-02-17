5b04938683c072b4f2c9a3ae4b43a4ad1987bb19
CarlosAndres <solrak41@gmail.com>
2023-02-17 15:30:09 -0500

    Add functionality to .add-book and .cancel buttons to show/hide popup form

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 let myLibrary = [];
 
 function Book() {
-  // the constructor...
+    // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
 
 const cardContainer = document.querySelector(".card-container");
 
 if (myLibrary.length === 0) {
-  const firstCard = document.createElement("div");
-  firstCard.textContent =
-    "Your library is empty. Click on the + icon to add your first book.";
-  firstCard.classList.add("first-card");
-  cardContainer.appendChild(firstCard);
+    const firstCard = document.createElement("div");
+    firstCard.textContent =
+        "Your library is empty. Click on the + icon to add your first book.";
+    firstCard.classList.add("first-card");
+    cardContainer.appendChild(firstCard);
 }
+
+const popUpForm = document.querySelector(".form-popup");
+
+const addButton = document.querySelector(".add-book");
+addButton.addEventListener("click", () => {
+    popUpForm.style.display = "grid";
+});
+
+const cancelButton = document.querySelector(".cancel");
+cancelButton.addEventListener("click", () => {
+    popUpForm.style.display = "none";
+});
