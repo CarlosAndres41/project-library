2e24e7af4dc7247a43c6aea1858259f421e67fda
CarlosAndres <solrak41@gmail.com>
2023-02-17 23:44:13 -0500

    Add status property to Book constructor

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.status = status;
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages.`;
     };
